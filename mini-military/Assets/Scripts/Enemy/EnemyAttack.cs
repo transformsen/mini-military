039ef3559ab8eccf578c4828ee6a19a50fd530b6
transformsen <senthilsct91@gmail.com>
2018-12-20 06:42:09 +0530

    Phase 1 Attacke enemey - 1

--- mini-military/Assets/Scripts/Enemy/EnemyAttack.cs
+++ mini-military/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -11,7 +11,7 @@ public class EnemyAttack : MonoBehaviour {
     Animator anim;                              // Reference to the animator component.
     GameObject player;                          // Reference to the player GameObject.
     PlayerHealth playerHealth;                  // Reference to the player's health.
-   // EnemyHealth enemyHealth;                    // Reference to this enemy's health.
+    EnemyHealth enemyHealth;                    // Reference to this enemy's health.
     bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
     float timer;                                // Timer for counting up to the next attack.
 
@@ -21,7 +21,7 @@ public class EnemyAttack : MonoBehaviour {
         // Setting up the references.
         player = GameObject.FindGameObjectWithTag("Player");
         playerHealth = player.GetComponent<PlayerHealth>();
-       // enemyHealth = GetComponent<EnemyHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
         anim = GetComponent<Animator>();
     }
 
@@ -54,7 +54,7 @@ public class EnemyAttack : MonoBehaviour {
         timer += Time.deltaTime;
 
         // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
-        if (timer >= timeBetweenAttacks && playerInRange /*&& enemyHealth.currentHealth > 0*/)
+        if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
         {
             // ... attack.
             Attack();
