476ef13155b548368558df6a5a0e2141e888a48c
transformsen <senthilsct91@gmail.com>
2018-12-28 00:08:56 +0530

    Phaes II New gun and First Aid

---
+++ mini-military/Assets/Scripts/FirstAid/FirstAidBoost.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAidBoost : MonoBehaviour {
+
+    GameObject player;                          // Reference to the player GameObject.
+    PlayerHealth playerHealth;                  // Reference to the player's health.
+
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the entering collider is the player...
+        
+        if (other.gameObject == player)
+        {            
+            playerHealth.currentHealth = playerHealth.startingHealth;
+        }
+    }
+    
+}
---
+++ mini-military/Assets/Scripts/FirstAid/FirstAidManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstAidManager : MonoBehaviour {
+
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    public GameObject firstAid;                // The enemy prefab to be spawned.
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(firstAid, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
