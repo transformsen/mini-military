75b7c0c03d6dc7653f594366501d6ceaa02e0ade
transformsen <senthilsct91@gmail.com>
2019-01-27 17:10:28 +0530

    Phase I Zoom and Gun Fixes

--- mini-military/Assets/Scripts/Camera/CameraFollow.cs
+++ mini-military/Assets/Scripts/Camera/CameraFollow.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
 
 public class CameraFollow : MonoBehaviour {
 
@@ -10,16 +12,21 @@ public class CameraFollow : MonoBehaviour {
     public float rotationSpeed = 4f;    // Spped of the camera to rotate.
     public float y = 5f;
     public float z = 6f;
+    public int maxZoom = 3;
+    public Text zoomXText;
 
     [Range(0.1f, 1.0f)]
     public float smoothFactor = 0.5f;
 
     Vector3 offset;                     // The initial offset from the target.
+    AudioSource zoomSound;
 
     public FixedTouchField touchField;
 
     float camaraAngle;
     public float caramaAngleSpeed = 1f;
+
+    public int currentZoom = 1;
 
     void Start()
     {
@@ -27,7 +34,9 @@ public class CameraFollow : MonoBehaviour {
         player = GameObject.FindGameObjectWithTag("Player");
         target = player.transform;
         offset = transform.position - target.position;
-        
+        zoomSound = GetComponent<AudioSource>();
+
+
     }
 
     void FixedUpdate()
@@ -38,10 +47,40 @@ public class CameraFollow : MonoBehaviour {
         // Smoothly interpolate between the camera's current position and it's target position.
         //transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
 
+        if (CrossPlatformInputManager.GetButtonDown("Jump")){
+            Debug.Log("Fire1");
+            zoomCalCulation();
+            zoomSound.Play();
+        }
+
+        if(zoomXText != null)
+        {
+            zoomXText.text = currentZoom + "X";
+        }
+        
+
         camaraAngle += touchField.TouchDist.x * caramaAngleSpeed;
 
         transform.position = target.position + Quaternion.AngleAxis(camaraAngle, Vector3.up) * new Vector3(0, y, z);
         transform.rotation = Quaternion.LookRotation(target.position + Vector3.up * 2f - transform.position, Vector3.up);
+    }
+
+    public void zoomCalCulation()
+    {
+        if(currentZoom < maxZoom)
+        {
+            currentZoom = currentZoom + 1;
+            Debug.Log("currentZoom=" + currentZoom);
+            y = y + 7f;
+            z = z + 7f;
+        }
+        else
+        {
+            currentZoom = 1;
+            y = y - 14f;
+            z = z - 14f;
+        }
+        
     }
 
     //private void LateUpdate()
